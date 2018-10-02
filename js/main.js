cf715a14ea0cb71f2a5060fcb87089703952f95e
Rayzr522 <rayzr522@gmail.com>
2018-10-02 17:35:47 -0400

    Added smooth-scrolling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
+// Fireflies
 $.firefly({
     on: 'header',
     color: '#fff',
     minPixel: 1,
     maxPixel: 3,
     total: 65
+});
+
+// Smooth scrolling
+$('a[href^="#"]').click(function () {
+    var anchor = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $('[name="' + anchor.substr(1) + '"]').offset().top
+    }, 750, "swing", function () {
+        window.location.href = anchor;
+    });
+
+    return false;
 });
